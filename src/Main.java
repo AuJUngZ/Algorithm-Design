98f13f9725d68bcca99e6d2e2c62c71ff70816ad
AuJung <natthaphong_th@cmu.ac.th>
2023-02-11 17:08:08 +0700

    fix something

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<String, Map<String, Integer>> graph = new HashMap<>();
-        Map<String, Map<String, Integer>> result = new HashMap<>();
         String start = "A";
 //        graph.put("A", new HashMap<>(Map.of("B", 0, "C", 10)));
 //        graph.put("B", new HashMap<>(Map.of("C", 2)));
