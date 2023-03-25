60d721dc25cc9cdc90c8a067509e09d956516df8
AuJung <natthaphong_th@cmu.ac.th>
2023-03-25 12:03:30 +0700

    - add random prime number

--- src/Final/RSA.java
+++ src/Final/RSA.java
@@ -2,33 +2,65 @@ package Final;
 
 
 public class RSA {
-    private final int p;
-    private final int q;
-    private int n;
-    private int z;
-    private int e;
-    private int d;
-    private final int[] publicKey;
-    private final int[] privateKey;
+    private final long p;
+    private final long q;
+    private final long[] publicKey;
+    private final long[] privateKey;
+    private long n;
+    private long z;
+    private long e;
+    private long d;
 
     // Constructor
     public RSA(int p, int q) {
+        if(p == q){
+            throw new IllegalArgumentException("p and q cannot be equal");
+        }
         this.p = p;
         this.q = q;
         this.findN();
         this.findZ();
         this.findE();
         this.findD();
-        this.publicKey = new int[]{this.e, this.n};
-        this.privateKey = new int[]{this.d, this.n};
+        this.publicKey = new long[]{this.e, this.n};
+        this.privateKey = new long[]{this.d, this.n};
+        System.out.println("Public key: " + this.publicKey[0] + ", " + this.publicKey[1]);
+        System.out.println("Private key: " + this.privateKey[0] + ", " + this.privateKey[1]);
     }
 
-    public int encrypt(int m) {
-        return (int) Math.pow(m, this.e) % this.n;
+    public static int randomPrime() {
+        int prime = 0;
+        do {
+            prime = (int) (Math.random() * 13);
+        } while (!isPrime(prime));
+        return prime;
     }
 
-    public int decrypt(int c) {
-        return (int) Math.pow(c, this.d) % this.n;
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2 || n == 3) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n) + 1;
+        for (int i = 3; i < sqrt; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public long encrypt(long m) {
+        return (long) Math.pow(m, this.e) % this.n;
+    }
+
+    public long decrypt(long c) {
+        return (long) Math.pow(c, this.d) % this.n;
     }
 
     private void findN() {
@@ -48,7 +80,7 @@ public class RSA {
         }
     }
 
-    private int gcd(int a, int b) {
+    private long gcd(long a, long b) {
         if (b == 0) {
             return a;
         }
